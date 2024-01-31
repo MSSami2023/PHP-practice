905ac296ea2175fa95085c984c65dfe02c66e16d
MSSami2023 <ms0727918@gmail.com>
2024-01-31 23:27:03 +0530

    All Operators 31-1-2024

---
+++ class2.php
@@ -0,0 +1,201 @@
+<?php
+
+// Arithmetic operator
+
+// $a = 5;
+// $b = 6;
+
+// echo $a+$b;
+// echo "<br>";
+
+// echo $a-$b;
+// echo "<br>";
+
+// echo $a*$b;
+// echo "<br>";
+
+// echo $a/$b;
+// echo "<br>";
+
+// echo $a%$b;
+// echo "<br>";
+
+// $a++;
+// echo "<br>";
+// echo $a;
+
+// $b--;
+// echo "<br>";
+// echo $b;
+
+
+// Comparison Operator
+
+// $a = "10";
+// $b = 10;
+
+// if($a==$b){
+//     echo "is True";
+// }else{
+//     echo "is False";
+// }
+
+// echo "<br>"
+
+// $a = "10";
+// $b = 10;
+
+// if($a===$b){
+//     echo "is True";
+// }else{
+//     echo "is False";
+// }
+
+// echo "<br>"
+
+
+// $a = 2;
+// $b = 20;
+
+// if($a!=$b){   
+//       echo "is True";
+//  }else{
+//       echo "is False";
+//     }
+
+
+    // echo "<br>"
+
+//     $a = 18;
+//     $b = 30;
+
+// if($a<$b){   
+//       echo "Eligible";
+//  }else{
+//       echo "Not Eligible";
+//     }
+
+    // echo "<br>"
+
+//     $a = 10;
+//     $b = 18;
+
+// if($a>$b){   
+//       echo "Eligible";
+//  }else{
+//       echo "Not Eligible";
+//     }
+
+
+// logical Operator
+
+// $a = "M.Sami";
+// $b = "Karachi123";
+
+// if($a=="M.Sami"&&$b=="Karachi123"){          
+//     echo"Id is Login";
+// }else{
+//     echo"Please Check Your user name and password";
+// }
+
+
+    // echo "<br>"
+
+
+//     $a = "M.Sami";
+//     $b = "Karachi123";
+
+// if($a=="M.Sami" || $b=="Karachi123"){          
+//     echo"Id is Login";
+// }else{
+//     echo"Please Check Your user name and password";
+// }
+
+
+//    echo "<br>"
+
+// $a = 10;
+// $b = 10;
+
+// if($a^$b){
+//     echo "Yes";
+// }else{
+//     echo "No";
+// }
+
+// echo "<br>"
+
+// $a = 0;
+
+// if(!$a){
+//     echo "True";
+// }else{
+//     echo "False";
+// }
+
+
+// Assignment Operator
+
+// $a = 5;
+// $b = 10;
+// $c = $a+$b;
+// echo $c;
+
+
+// echo "<br>"
+
+// $a = 5;
+// $b = 10;
+// $b +=$a;
+// echo $b;
+
+
+// echo "<br>"
+
+
+// $a = 5;
+// $b = 10;
+// $b -=$a;
+// echo $b;
+
+// echo "<br>"
+
+
+// $a = 5;
+// $b = 10;
+// $b *=$a;
+// echo $b;
+
+// echo "<br>"
+
+
+// $a = 5;
+// $b = 10;
+// $b /=$a;
+// echo $b;
+
+// echo "<br>"
+
+
+
+// $a = 23;
+// $b = 3;
+// $a %=$b;
+// echo $a;
+
+
+// echo "<br>"
+
+
+// $a = 23;
+// $b = 2;
+// $c = $a%$b;
+// echo $c;
+
+
+// echo "<br>"
+
+
+
+
+?>
