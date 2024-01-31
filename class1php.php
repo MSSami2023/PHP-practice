4c29aabe94ff7e01d9e4090badbeeba87e187a51
MSSami2023 <ms0727918@gmail.com>
2024-01-31 22:18:54 +0530

    29-jan-2024

---
+++ class1php.php
@@ -0,0 +1,20 @@
+<?php
+$a = 19; //Int
+$b = 10.10; //Float
+$c = true; //Boolean
+$d = "Shan,Ali"; //String
+$color = ["black","blue","red"]; //Array
+
+// echo $a."<br>".$b."<br>".$c."<br>".$d."<br>";
+// print_r($color);
+
+
+// Data type Check
+// var_dump($d)
+
+// character check
+// echo strlen($d)
+
+// word count
+// echo str_word_count($d)
+?>
